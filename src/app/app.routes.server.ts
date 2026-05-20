039c4af96106b543e8b04409fb08e1f2e7eccfd2
alex1608-cell <alejc2006@gmail.com>
2026-05-20 10:58:36 +0200

    cambios output

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,30 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'alumno',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'profesores',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'asignaturas',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'matriculas',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'matriculasAprobadas',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server
   }
